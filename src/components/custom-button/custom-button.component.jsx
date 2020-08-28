ae94855790c880faf4c43274632cadde0794ce5c
nivdungebhagyashri <vibha.nivdunge@gmail.com>
2020-08-27 23:23:17 -0700

    Update customButton

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './custom-button.styles.scss';
 
+// this is custom button so that we can use it at multiple places
 const CustomButton = ({children, ...otherProps}) =>(
    <div>
        <button className ='custom-button' type='submit' {...otherProps}>
