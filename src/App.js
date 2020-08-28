f9acf92b61049e03c80674427665742cef36ab61
nivdungebhagyashri <vibha.nivdunge@gmail.com>
2020-08-28 02:16:32 -0700

    Implemented FireBase utils to save email and password user

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import ShopPage from './pages/shop/shop.component';
 import {Route, Switch} from 'react-router-dom';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import SnapshotState from 'jest-snapshot/build/State';
+import { SNAPSHOT_VERSION } from 'jest-snapshot/build/utils';
 
 class App extends React.Component {
   constructor(){
@@ -15,15 +17,32 @@ class App extends React.Component {
       currentUser: null
     }
   }
+  unSubscribeFromAuth= null;
 
   componentDidMount(){
-      auth.onAuthStateChanged(user => {
-        this.setState({currentUser:user});
-        console.log(user);
+    this.unSubscribeFromAuth = 
+      auth.onAuthStateChanged(async userAuth => {
+        if(userAuth){
+          const userRef = await createUserProfileDocument(userAuth);
+          userRef.onSnapshot(snapShot => {
+            this.setState({
+              currentUser:{
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            }
+            ,() => {console.log(this.state)
+            }
+            );
+          }
+          );
+          
+        }
+        this.setState({currentUser: userAuth});
+        
+
       });
   }
-
-  unSubscribeFromAuth= null;
 
   componentWillUnmount(){
     this.unSubscribeFromAuth();
